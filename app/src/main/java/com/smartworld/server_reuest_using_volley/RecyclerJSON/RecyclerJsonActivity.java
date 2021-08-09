560c046714035f1f3e86d07f80fc73c568b68cc0
prantae-desktop-005 <mailmeinmymail0@gmail.com>
2021-08-09 14:25:30 +0530

    New Update

---
+++ app/src/main/java/com/smartworld/server_reuest_using_volley/RecyclerJSON/RecyclerJsonActivity.java
@@ -0,0 +1,16 @@
+package com.smartworld.server_reuest_using_volley.RecyclerJSON;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.smartworld.server_reuest_using_volley.R;
+
+public class RecyclerJsonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_json);
+    }
+}
